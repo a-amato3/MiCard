5da21678b1052d0faa115285a02037e2792ef0d6
Aaron Amato <amato@pnptc.com>
2020-09-10 15:18:59 +0100

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.orange,
+            title: Text("Demo"),
+          ),
+          body: Center(
+            child: Text("My name is aaron."),
+          ),
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.red,
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
